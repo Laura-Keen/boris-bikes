5ff7040074c6cfd1247fc273f5fe73e4f707577a
Laura <keenlaura@hotmail.co.uk>
2021-02-02 17:07:14 +0000

    Completed step 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,11 @@
 require 'docking_station.rb'
 
+# describe DockingStation do
+#   it 'responds to release_bike' do
+#     expect(subject).to respond_to :release_bike
+#   end
+# end
+
 describe DockingStation do
-
-
+  it {is_expected.to respond_to :release_bike}
 end
